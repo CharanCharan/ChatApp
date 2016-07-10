c19aa0cea6f6aeead9eae86c9efeeeb085605908
CharanCharan <cherry1993series@gmail.com>
2016-07-10 09:10:25 +0530

    added time stamp

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+//add timestamps in front of log messages
+require('console-stamp')(console, '[HH:MM:ss.l]');
+
  var express = require('express');
  var app = express();
  var server = require('http').createServer(app);
  var io = require('socket.io').listen(server);
+
+//since logger only returns a UTC version of date, I'm defining my own date format - using an internal module from console-stamp
+var logger = require('morgan');
+
+logger.format('mydate', function() {
+    var df = require('console-stamp/node_modules/dateformat');
+    return df(new Date(), 'HH:MM:ss.l');
+});
+app.use(logger('[:mydate] :method :url :status :res[content-length] - :remote-addr - :response-time ms'));
+
  users = [];
  connections =[];
 
